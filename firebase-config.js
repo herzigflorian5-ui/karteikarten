fff314cf618f1cacaa1198311e93ba824720b761
herzigflorian5-ui <herzigflorian5@gmail.com>
2026-08-15 22:29:32 +0200

    Add Firebase config for GitHub Pages deployment

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey:            "AIzaSyAoGfB9P468tep4_Xt7ac2cI4Cn33inlKc",
+  authDomain:        "karteikarten-10166.firebaseapp.com",
+  projectId:         "karteikarten-10166",
+  storageBucket:     "karteikarten-10166.firebasestorage.app",
+  messagingSenderId: "229357100171",
+  appId:             "1:229357100171:web:e9d13689cf5691066b888e",
+};
